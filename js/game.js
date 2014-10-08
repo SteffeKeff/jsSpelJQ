6269b8ffdd7d3841cb5142cda5b06ec064c702b7
Stefan De Geer <sdgkeff@gmail.com>
2014-10-09 00:07:00 +0200

    påbörjat mobil-knapparna, fungerar ej upp/ned

--- js/game.js
+++ js/game.js
@@ -6,12 +6,24 @@
 // Licence:     GNU GPL
 /////////////////////////////////////////////////////////////////////////////
 var t1, t2, d1, d2, time, start, runTime, level;
-level = jQuery.extend(true, {}, originLevel);
+level = jQuery.extend(true, {}, originLevel); //copy the originLevel to level
+//check if mobile user
+var mobile = $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
 $(function () {
 	'use strict';
 	for (var i = 0; i < level.map.length; i++) {
 		$('#level').append('<option value="' + i + '">Level ' + (i + 1) + '</option>');
 	}
+	var click = function (button) {
+		if (!start) {
+			d1 = new Date();
+			t1 = d1.getTime();
+			start = true;
+			runTime = true;
+		}
+		logic.askMoveLocation(button);
+		ui.update(level);
+	};
 	start = false;
 	player.setStartPos(level.playerStartX[level.current], level.playerStartY[level.current]);
 	$('#play').on('click', function () {
@@ -29,6 +41,27 @@ $(function () {
 			$('#gameBoard').addClass('center');
 			ui.update(level);
 			$('#game').append('<p id="timer">Timer: 0.00</p>');
+			if (mobile) {
+				$('#game').append('<div id="buttons"></div>');
+				$('#buttons').append('<button id="button-up" class="arrow-key">⇡</button>');
+				$('#buttons').append('<button id="button-left" class="arrow-key">⇠</button>');
+				$('#buttons').append('<button id="button-down" class="arrow-key">⇣</button>');
+				$('#buttons').append('<button id="button-right" class="arrow-key">⇢</button>');
+				$('#button-left').on('click', function () {
+					click(37);
+				});
+				$('#button-right').on('click', function () {
+					console.log('right');
+					click(39);
+				});
+				$('#button-up').on('click', function () {
+					console.log('up');
+					click(38);
+				});
+				$('#button-down').on('click', function () {
+					click(40);
+				});
+			}
 			var myTimer = function () {
 				if (runTime) {
 					d2 = new Date();
