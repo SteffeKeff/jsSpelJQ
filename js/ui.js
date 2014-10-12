f35be6682dce9c1add09562e8a7c3e4ecee08ba7
Stefan De Geer <sdgkeff@gmail.com>
2014-10-13 00:41:28 +0200

    småfix

--- js/ui.js
+++ js/ui.js
@@ -33,10 +33,8 @@ ui.update = function (playlevel) {
 		}
 	}
 };
-ui.fillList = function(id, length, text)
-{
-	for (var i = 0; i < length; i++) 
-	{
-  		$(id).append('<option value="' + i + '">'+ text + (i + 1) + ' </option>');
- 	}
-}
+ui.fillList = function (id, length, text) {
+	for (var i = 0; i < length; i++) {
+		$(id).append('<option value="' + i + '">' + text + (i + 1) + ' </option>');
+	}
+};
