4f152ca5809f941aec854c196738f02d7ca504cb
Stefan De Geer <sdgkeff@gmail.com>
2014-10-13 01:04:19 +0200

    fixat in allt i game.run och game.tutorial

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ game.validateName = function () {
 game.run = function () {
 	if (game.validateName()) {
 		game.playlevel = level.getCopy();
-		playlevel.current = $('#level').val();
+		game.playlevel.current = $('#level').val();
 		player.setStartPosFromLevel();
 		timer.stop();
 		$('#invalid').text('');
@@ -65,76 +65,48 @@ game.run = function () {
 			});
 		}
 	}
+	var myTimer = function () {
+		if (runTime) {
+			d2 = new Date();
+			t2 = d2.getTime();
+			time = (((t2 - t1) / 1000)).toFixed(2);
+		}
+		if (t1 !== undefined) {
+			$("#timer").text('Timer: ' + time);
+		}
+	};
+	var myVar = setInterval(function () {
+		myTimer();
+	}, 100);
+	$(window).on('keydown', function (e) {
+		if (!start) {
+			d1 = new Date();
+			t1 = d1.getTime();
+			start = true;
+			runTime = true;
+		}
+		logic.askMoveLocation(e.keyCode);
+		ui.update(game.playlevel);
+	});
+};
+game.tutorial = function () {
+	$('#menu').fadeOut();
+	$('#tutorial').show();
+	$('#next').on('click', function () {
+		$('#tutorial').fadeOut();
+		$('#menu').fadeIn();
+	});
 };
 
 $(function () {
 	'use strict';
 	ui.fillList('#level', game.playlevel.map.length, 'Level');
 	player.setStartPosFromLevel();
-	//player.setStartPosFromLevel();
-	//player.setStartPos(playlevel.playerStartX[playlevel.current], playlevel.playerStartY[playlevel.current]);
+
 	$('#play-button').on('click', function () {
-		player.name = $('#name').val().trim();
-		if (player.name === '') {
-			$('#invalid').text('your name is not valid');
-		} else {
-			game.playlevel = jQuery.extend(true, {}, level); //copy the originLevel to level
-			game.playlevel.current = $('#level').val();
-			player.setStartPos(game.playlevel.playerStartX[game.playlevel.current], game.playlevel.playerStartY[game.playlevel.current]);
-			time = 0;
-			$('#invalid').text('');
-			$('#menu').fadeOut();
-			$('#player-name').text('Player name: ' + player.name);
-			$('#game').show();
-			ui.update(game.playlevel);
-			if (ui.isMobile()) {
-				$('#buttons').show();
-				$('#button-left').on('click', function () {
-					click(37);
-				});
-				$('#button-right').on('click', function () {
-					console.log('right');
-					click(39);
-				});
-				$('#button-up').on('click', function () {
-					console.log('up');
-					click(38);
-				});
-				$('#button-down').on('click', function () {
-					click(40);
-				});
-			}
-			var myTimer = function () {
-				if (runTime) {
-					d2 = new Date();
-					t2 = d2.getTime();
-					time = (((t2 - t1) / 1000)).toFixed(2);
-				}
-				if (t1 !== undefined) {
-					$("#timer").text('Timer: ' + time);
-				}
-			};
-			var myVar = setInterval(function () {
-				myTimer();
-			}, 100);
-			$(window).on('keydown', function (e) {
-				if (!start) {
-					d1 = new Date();
-					t1 = d1.getTime();
-					start = true;
-					runTime = true;
-				}
-				logic.askMoveLocation(e.keyCode);
-				ui.update(game.playlevel);
-			});
-		}
+		game.run();
 	});
 	$('#tutorial-button').on('click', function () {
-		$('#menu').fadeOut();
-		$('#tutorial').show();
-		$('#next').on('click', function () {
-			$('#tutorial').fadeOut();
-			$('#menu').fadeIn();
-		});
+		game.tutorial();
 	});
 });
