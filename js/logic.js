321c35d3f7338c3b4366464eb466598e20837899
Stefan De Geer <sdgkeff@gmail.com>
2014-10-13 03:03:48 +0200

    tagit bort sleep som inte används

--- js/logic.js
+++ js/logic.js
@@ -122,12 +122,3 @@ logic.gameWon = function () {
         });
     }
 };
-
-logic.sleep = function (milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds) {
-            break;
-        }
-    }
-};
